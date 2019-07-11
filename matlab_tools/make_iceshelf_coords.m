0571df6147d5d8161a109a7fbbdb56a361645d95
Eva <evacougnon@gmail.com>
2019-07-11 10:49:41 +1000

    extract ice shelf coordinates using antbounds toolbox from Chad Green

--- matlab_tools/make_iceshelf_coords.m
+++ matlab_tools/make_iceshelf_coords.m
@@ -3,14 +3,18 @@
 % Stolen from Ole Richter at:
 % https://github.com/kuechenrole/antarctic_melting/blob/master/matlab_scripts/make_iceshelf_coords.m
 %
-% adapted by Eva Cougnon on June 2019
+% adapted by Eva Cougnon 
+% created: June 2019
+% last modif: July 2019
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% Chad Green toolbox
 addpath AntarcticMappingTools/
 addpath antbounds/
 
-load iceshelves_2008_v2.mat
+% from the antbounds toolbox (Chad Green)
+load antbounds/iceshelves_2008_v2.mat
 
 [xgrid,ygrid] = psgrid(-90,0,5510,5,'xy');
 %plot(xgrid,ygrid,'.','color',0.8*[1 1 1])
